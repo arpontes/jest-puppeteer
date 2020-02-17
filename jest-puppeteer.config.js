4c47eb59dafffe4adcc752477acf3eec726cfde7
mtzcpd1809 <andresilva@ext.cvc.com.br>
2020-02-17 15:13:24 -0300

    Inclusão do básico para testes de regressão

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    server: {
+        command: "npm run start",
+        port: 3000,
+        launchTimeout: 60000
+    },
+    launch: {
+        headless: false
+      }
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+async function runTests(page) {
+    await page.goto("http://localhost:3000");
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    await page.evaluate(() => {
+        (document.querySelectorAll(".App-logo") || []).forEach(el => el.remove());
+    });
+
+    let image = await page.screenshot();
+    //Acabou de carregar
+    expect(image).toMatchImageSnapshot();
+}
+
+describe("Medium Viewport", () => {
+    it("Render loading", async () => {
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1024, height: 768 });
+        await runTests(page);
+    });
+});
+
+describe("Small Viewport", () => {
+    it("Render loading", async () => {
+        const page = await browser.newPage();
+        await page.setViewport({ width: 360, height: 640 }); // Galaxy S5
+        await runTests(page);
+    });
 });
